813c33beb1f36065ed59c329b6f349205d00c942
mgupta8143 <67887655+mgupta8143@users.noreply.github.com>
2020-08-22 11:50:51 -0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print("[NEW CONNECTION] {} is connected.".format(addr))
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print("{} {}".format(addr,msg))
+    conn.close()
+
+def start():
+    print("[STARTING] Server is starting...")
+    server.listen()
+    print("[LISTENING] Server is listening on {}".format(SERVER))
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target = handle_client, args = (conn, addr))
+        thread.start()
+        print("[ACTIVE CONNECTIONS] {} connections active.".format(threading.activeCount() - 1))
+   
+start()
